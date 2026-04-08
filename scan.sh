1b268f310e0e9937ecf725efca0dc1f63c542e56
s0i37 <s0i37@notebook>
2026-04-08 12:15:05 +0500

    updated scan.sh

--- scan.sh
+++ scan.sh
@@ -8,42 +8,48 @@ BLUE='\x1b[34m'
 GREY='\x1b[90m'
 RESET='\x1b[0m'
 
-[[ $# -ge 1 ]] && interval="$1" || interval=15
+[[ $# -ge 1 ]] && filter="$1" || filter=''
 [ -z "$IFACE" ] && IFACE='wlan0'
+[ -z "$INTERVAL" ] && INTERVAL=0
 
+shopt -s lastpipe
 sudo ifconfig $IFACE up
 while :; do
   date +'%H:%M:%S %d.%m.%Y'
-  ap=''; essid=''; auth=$RED'OPN'$RESET; wps=''
-  sudo iw dev $IFACE scan | egrep '^BSS|SSID:|freq:|signal:|TSF:|Authentication|WPS:|set: channel' | while read -r line
-  do #echo -e "$line"
-    if echo "$line" | egrep -q '^BSS' && [ -n "$ap" ]; then
-      vendor=$(mac_vendor_lookup $ap | grep -v 'Prefix is not registered')
-      echo -e "$ap [${YELLOW}$vendor${RESET}] ${CYAN}$essid${RESET} ${BLUE}$signal${RESET} $channel $auth (${RED}$wps${RESET})"
-      ap=''; essid=''; auth=$RED'OPN'$RESET; wps=''
+  { sudo iw dev $IFACE scan | egrep '^BSS|SSID:|freq:|signal:|TSF:|capability:|Authentication|WPS:' | while read -r line
+  do #echo -e "$line" >&2
+  if echo "$line" | egrep -q '^BSS' && [ -n "$out" ]; then
+    echo "$out"
+    out=""
+  fi
+  if echo "$line" | egrep -q 'Authentication|WPS:' && ! echo "$out" | grep -q 'SSID:'; then out+=$'SSID:\t'; fi
+  out+="$line"$'\t'
+  done; echo "$out"; } | grep -i "$filter" | while IFS=$'\t' read -r ap uptime freq caps signal essid auth wps
+  do #echo "ap=$ap, uptime=$uptime, signal=$signal, essid=$essid, channel=$channel, capabilities=$caps, auth=$auth, wps=$wps" >&2
+    ap=$(echo "$ap" | awk '{print $2}' | cut -d '(' -f 1)
+    vendor=$(mac_vendor_lookup $ap | grep -v 'Prefix is not registered') #pip3 install mac-vendor-lookup
+    read -r _ essid <<< $(echo "$essid" | grep 'SSID:')
+    freq=$(echo "$freq" | grep 'freq:' | awk '{print $2}')
+    signal=$(echo "$signal" | grep 'signal:' | awk '{print $2}' | cut -d '.' -f 1)
+    uptime=$(echo "$uptime" | grep 'TSF:' | awk '{print $4 " " $5}')
+    if echo "$auth" | grep -q '802.1'; then
+      auth=$RED'EAP'$RESET
+    elif echo "$auth" | grep -q 'SAE'; then
+      auth=$GREEN'WPA3'$RESET
+    elif echo "$auth" | grep -q 'PSK'; then
+      auth=$GREEN'WPA'$RESET
+    elif echo "$caps" | grep -q 'Privacy'; then
+      auth=$RED'WEP'$RESET
+    else
+      auth=$RED'OPN'$RESET
     fi
-    if echo "$line" | egrep -q '^BSS'; then
-      ap=$(echo "$line" | egrep '^BSS' | awk '{print $2}' | cut -d '(' -f 1)
-    elif echo "$line" | grep -q 'SSID:'; then
-      read -r _ essid <<< $(echo "$line" | grep 'SSID:')
-    elif echo "$line" | grep -q 'freq:'; then
-      freq=$(echo "$line" | grep 'freq:' | awk '{print $2}')
-    elif echo "$line" | grep -q 'signal:'; then
-      signal=$(echo "$line" | grep 'signal:' | awk '{print $2}' | cut -d '.' -f 1)
-    elif echo "$line" | grep -q 'TSF:'; then
-      uptime=$(echo "$line" | grep 'TSF:' | awk '{print $4 " " $5}')
-    elif echo "$line" | grep -q 'Authentication'; then
-      if echo "$line" | grep -q '802.1'; then
-        auth=$RED'EAP'$RESET
-      elif echo "$line" | grep -q 'PSK'; then
-        auth=$GREEN'WPA'$RESET
-      fi
-    elif echo "$line" | grep -q 'WPS:'; then
-      read _ _ wps <<< $(echo "$line" | grep 'WPS:')
-    elif echo "$line" | grep -q 'set: channel'; then
-      channel=$(echo "$line" | grep 'set: channel' | awk '{print $5}')
+    if echo "$wps" | grep -q 'WPS:'; then
+      wps='WPS'
+    else
+      wps=''
     fi
+    echo -e "$ap [${YELLOW}$vendor${RESET}] ${CYAN}$essid${RESET} ${BLUE}$signal${RESET} $freq $auth ${RED}$wps${RESET}"
   done
   echo ''
-  sleep "$interval"
+  sleep "$INTERVAL"
 done
